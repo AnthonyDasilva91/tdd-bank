a2fd3c3d25f938803173a822b906a77dd3d224ac
Anthony <anthony.dasilvafernandes91@gmail.com>
2020-10-12 23:15:19 +0200

    WithdrawUseCase should throw exception when amount is zero : KO

---
+++ usecase/src/test/java/com/tddbank/usecase/WithdrawUseCaseTest.java
@@ -0,0 +1,38 @@
+package com.tddbank.usecase;
+
+import com.tddbank.domain.entity.Account;
+import com.tddbank.domain.exception.NotValidAmountException;
+import com.tddbank.usecase.port.AccountRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class WithdrawUseCaseTest {
+
+    private AccountRepository mockAccountRepository;
+
+    @BeforeEach
+    void mock() {
+        mockAccountRepository = Mockito.mock(AccountRepository.class);
+    }
+
+    @Test
+    void should_throw_exception_when_withdraw_is_zero() {
+
+        // Arrange
+        UUID existingAccountId = UUID.randomUUID();
+        Mockito.when(mockAccountRepository.findById(existingAccountId)).thenReturn(Optional.of(new Account()));
+
+        WithdrawUseCase withdrawUseCase = new WithdrawUseCase(mockAccountRepository);
+
+        // Act & Assert
+        Assertions.assertThrows(NotValidAmountException.class, () -> withdrawUseCase.withdraw(existingAccountId, 0));
+    }
+}
