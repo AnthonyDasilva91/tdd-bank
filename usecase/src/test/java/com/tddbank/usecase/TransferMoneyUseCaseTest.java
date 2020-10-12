8ecc2c26e7e10e1ef3f61ab814bbaf48aa33d98c
Anthony <anthony.dasilvafernandes91@gmail.com>
2020-10-13 00:28:06 +0200

    Transfer money from payer to payee : KO

--- usecase/src/test/java/com/tddbank/usecase/TransferMoneyUseCaseTest.java
+++ usecase/src/test/java/com/tddbank/usecase/TransferMoneyUseCaseTest.java
@@ -1,7 +1,10 @@
 package com.tddbank.usecase;
 
+import com.tddbank.domain.entity.Account;
 import com.tddbank.domain.exception.AccountNotFoundException;
 import com.tddbank.usecase.account.CreateAccountUseCase;
+import com.tddbank.usecase.account.GetAccountUseCase;
+import com.tddbank.usecase.money.DepositUseCase;
 import com.tddbank.usecase.money.TransferMoneyUseCase;
 import com.tddbank.usecase.port.AccountRepository;
 import com.tddbank.usecase.port.AccountRepositoryImpl;
@@ -9,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TransferMoneyUseCaseTest {
@@ -24,7 +28,8 @@ public class TransferMoneyUseCaseTest {
         UUID payeeAccountId = createAccountUseCase.create().getId();
 
         // Act & Assert
-        assertThrows(AccountNotFoundException.class, () -> transferMoneyUseCase.transfer(notExistingPayerId, payeeAccountId, 10));
+        assertThrows(AccountNotFoundException.class,
+                () -> transferMoneyUseCase.transfer(notExistingPayerId, payeeAccountId, 10));
     }
 
     @Test
@@ -38,6 +43,38 @@ public class TransferMoneyUseCaseTest {
         UUID payerAccountId = createAccountUseCase.create().getId();
 
         // Act & Assert
-        assertThrows(AccountNotFoundException.class, () -> transferMoneyUseCase.transfer(payerAccountId, notExistingPayeeId, 10));
+        assertThrows(AccountNotFoundException.class,
+                () -> transferMoneyUseCase.transfer(payerAccountId, notExistingPayeeId, 10));
+    }
+
+    @Test
+    void should_throw_exception_when_payee_not_exist() {
+
+        // Arrange
+        AccountRepository accountRepository = new AccountRepositoryImpl();
+        CreateAccountUseCase createAccountUseCase = new CreateAccountUseCase(accountRepository);
+        DepositUseCase depositUseCase = new DepositUseCase(accountRepository);
+        GetAccountUseCase getAccountUseCase = new GetAccountUseCase(accountRepository);
+        TransferMoneyUseCase transferMoneyUseCase = new TransferMoneyUseCase(accountRepository);
+
+        UUID payerAccountId = createAccountUseCase.create().getId();
+        UUID payeeAccountId = createAccountUseCase.create().getId();
+
+        // Add money on both accounts
+        depositUseCase.deposit(payerAccountId, 100);
+        depositUseCase.deposit(payeeAccountId, 100);
+
+        double expectedPayerAmount = 90;
+        double expectedPayeeAmount = 110;
+
+        // Act
+        transferMoneyUseCase.transfer(payerAccountId, payeeAccountId, 10);
+
+        // Assert
+        Account payer = getAccountUseCase.get(payerAccountId);
+        Account payee = getAccountUseCase.get(payeeAccountId);
+
+        assertEquals(expectedPayerAmount, payer.getAmount());
+        assertEquals(expectedPayeeAmount, payee.getAmount());
     }
 }
