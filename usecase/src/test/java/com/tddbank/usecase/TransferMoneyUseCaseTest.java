f9123983b51e4d917f5f960fcee8fe20980d23e4
Anthony <anthony.dasilvafernandes91@gmail.com>
2020-10-13 00:18:59 +0200

    TransferMoneyUseCase should throw exception when payee does not exist: KO

--- usecase/src/test/java/com/tddbank/usecase/TransferMoneyUseCaseTest.java
+++ usecase/src/test/java/com/tddbank/usecase/TransferMoneyUseCaseTest.java
@@ -26,4 +26,18 @@ public class TransferMoneyUseCaseTest {
         // Act & Assert
         assertThrows(AccountNotFoundException.class, () -> transferMoneyUseCase.transfer(notExistingPayerId, payeeAccountId, 10));
     }
+
+    @Test
+    void should_throw_exception_when_payee_not_exists() {
+        // Arrange
+        AccountRepository accountRepository = new AccountRepositoryImpl();
+        CreateAccountUseCase createAccountUseCase = new CreateAccountUseCase(accountRepository);
+        TransferMoneyUseCase transferMoneyUseCase = new TransferMoneyUseCase(accountRepository);
+
+        UUID notExistingPayeeId = UUID.randomUUID();
+        UUID payerAccountId = createAccountUseCase.create().getId();
+
+        // Act & Assert
+        assertThrows(AccountNotFoundException.class, () -> transferMoneyUseCase.transfer(payerAccountId, notExistingPayeeId, 10));
+    }
 }
